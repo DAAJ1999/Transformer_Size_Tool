236ee6dc4a15e6c25545b61c9242de93da924d3a
DAAJ1999 <81645369+DAAJ1999@users.noreply.github.com>
2023-03-05 16:17:02 -0500

    u
    u

--- scripts.js
+++ scripts.js
@@ -1602,34 +1602,14 @@ if(Met=="Coincidence Factor"){
      
 	}
 	}
-
+	const evs = document.querySelectorAll('.EVs');
+	const count = evs.length;
+	var total=0;
+	for (let i = 0; i < count; i++) {
+		total=total+parseInt(evs[i].value);
+	}
+	console.log(averageEVS/count);
 	
+
 	return false;
-}
- 
-
-
-
-        function addFields(){
-            // Generate a dynamic number of inputs
-            var number = document.getElementById("InputnumberofhousesElectricV").value;
-            // Get the element where the inputs will be added to
-            var container = document.getElementById("container");
-            // Remove every children it had before
-            while (container.hasChildNodes()) {
-                container.removeChild(container.lastChild);
-            }
-            for (i=0;i<number;i++){
-                // Append a node with a random text
-                container.appendChild(document.createTextNode("" + (i+1)));
-                // Create an <input> element, set its type and name attributes
-                var input = document.createElement("input");
-                input.type = "number";
-                input.name = "House" + i +"EV Charger Value";
-				input.id= "HouseEVC" + i;
-                container.appendChild(input);
-                // Append a line break 
-                container.appendChild(document.createElement("br"));
-            }
-        }
-        }
+}  

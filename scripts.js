c41132218b942cacb62a26f6e33003e7ebca2a79
DAAJ1999 <81645369+DAAJ1999@users.noreply.github.com>
2023-04-01 16:26:45 -0400

    u
    u

--- scripts.js
+++ scripts.js
@@ -19,7 +19,35 @@ function fn1()
 	var LSF= document.getElementById("SquareFootSize1").value;
 	var Cost= 0;
 	var TS = 0;
-	var cf= [
+	var cf= [0,1,0.85,0.74,0.66,0.61,0.57,0.54,0.52,0.5,0.49,0.47,0.46,0.45,0.43,0.42,0.41,0.39,0.38,0.38,0.37,0.37];
+	const Kwhs = document.querySelectorAll('.KWs');
+	const count1 = Kwhs.length;
+	var total1=0;
+	for (let i = 0; i < count1; i++) {
+		total1=total1+parseFloat(Kwhs[i].value);
+	}
+	console.log(total1/count1);
+	if(count1 > 0){
+	var sum1= total1/count1;
+	}
+	else{
+		var sum1= 0;
+	}
+
+	const Kwhw = document.querySelectorAll('.KWw');
+	const count2 = Kwhw.length;
+	var total2=0;
+	for (let i = 0; i < count2; i++) {
+		total1=total2+parseFloat(Kwhw[i].value);
+	}
+	console.log(total2/count2);
+	if(count2 > 0){
+	var sum2= total2/count2;
+	}
+	else{
+		var sum2= 0;
+	}
+	
 	const evs = document.querySelectorAll('.EVs');
 	const count = evs.length;
 	var total=0;
@@ -1582,7 +1610,314 @@ if(Met=="Diversity"){
 }
 	
 if(Met=="Coincidence Factor"){
-}
+	 
+		
+	
+		var CFA= cf[NH];
+		
+		var ES= (Esummer*NHE)+(sum*count);
+		var EW= (Ewinter*NHE)+(sum*count);
+		var GS= (Gsummer*NHG)+(sum*count);
+		var GW= (Gwinter*NHG)+(sum*count);
+		
+		var ESDF = ES*DFA;
+		var EWDF = EW*DFA;
+		var GSDF = GS*DFA;
+		var GWDF = GW*DFA;
+			
+			
+     if((ESDF < 14 || ESDF == 14 && ESDF > 0 ) && (EWDF < 16 || EWDF == 16 && EWDF > 0)){
+			TS = 10;
+			document.getElementById("TS").innerHTML= "Transformer Size:"+TS+"KVA";
+			console.log(TS);
+			
+	  if (TS== 5){
+		  Cost="$"+7475.11+"USD";
+		   document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 10){
+		  Cost="$"+7921.34+"USD";
+		   document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 15){
+		  Cost="$"+8850.54+"USD";
+		   document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 25){
+		  Cost="$"+10226.66+"USD";
+		   document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 37.5){
+		  Cost="$"+13641.59+"USD";
+		   document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 50){
+		  Cost="$"+14171.88+"USD";
+		   document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 75){
+		  Cost="$"+15308.08+"USD";
+		  document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 100){
+		  Cost="$"+18556.83+"USD";
+		 document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 167){
+		  Cost="$"+21021.43+"USD";
+		 document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  
+     
+	}
+		}else if((ESDF < 21 || ESDF == 21 && ESDF > 14) && (EWDF < 24 || EWDF == 24 && EWDF > 16)){
+			TS = 15;
+			document.getElementById("TS").innerHTML= "Transformer Size:"+TS+"KVA";
+			console.log(TS);
+			
+			
+	  if (TS== 5){
+		  Cost="$"+7475.11+"USD";
+		   document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 10){
+		  Cost="$"+7921.34+"USD";
+		   document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 15){
+		  Cost="$"+8850.54+"USD";
+		   document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 25){
+		  Cost="$"+10226.66+"USD";
+		   document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 37.5){
+		  Cost="$"+13641.59+"USD";
+		   document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 50){
+		  Cost="$"+14171.88+"USD";
+		   document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 75){
+		  Cost="$"+15308.08+"USD";
+		  document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 100){
+		  Cost="$"+18556.83+"USD";
+		 document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 167){
+		  Cost="$"+21021.43+"USD";
+		 document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  
+     
+	}
+		}else if((ESDF < 35 || ESDF == 35 && ESDF > 21) && (EWDF < 40 || EWDF == 40 && EWDF > 24)){
+			TS = 25;
+			document.getElementById("TS").innerHTML= "Transformer Size:"+TS+"KVA";
+			console.log(TS);
+			
+	  if (TS== 5){
+		  Cost="$"+7475.11+"USD";
+		   document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 10){
+		  Cost="$"+7921.34+"USD";
+		   document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 15){
+		  Cost="$"+8850.54+"USD";
+		   document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 25){
+		  Cost="$"+10226.66+"USD";
+		   document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 37.5){
+		  Cost="$"+13641.59+"USD";
+		   document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 50){
+		  Cost="$"+14171.88+"USD";
+		   document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 75){
+		  Cost="$"+15308.08+"USD";
+		  document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 100){
+		  Cost="$"+18556.83+"USD";
+		 document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 167){
+		  Cost="$"+21021.43+"USD";
+		 document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  
+     
+	}
+		}else if((ESDF < 53 || ESDF == 53 && ESDF > 35) && (EWDF < 60 || EWDF == 60 && EWDF > 40)){
+			TS = 37.5;
+			document.getElementById("TS").innerHTML= "Transformer Size:"+TS+"KVA";
+			console.log(TS);
+			
+			
+	  if (TS== 5){
+		  Cost="$"+7475.11+"USD";
+		   document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 10){
+		  Cost="$"+7921.34+"USD";
+		   document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 15){
+		  Cost="$"+8850.54+"USD";
+		   document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 25){
+		  Cost="$"+10226.66+"USD";
+		   document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 37.5){
+		  Cost="$"+13641.59+"USD";
+		   document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 50){
+		  Cost="$"+14171.88+"USD";
+		   document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 75){
+		  Cost="$"+15308.08+"USD";
+		  document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 100){
+		  Cost="$"+18556.83+"USD";
+		 document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 167){
+		  Cost="$"+21021.43+"USD";
+		 document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  
+     
+	}
+		}else if((ESDF < 70 || ESDF == 70 && ESDF > 53) && (EWDF < 80 || EWDF == 80 && EWDF > 60)){
+			TS = 50;
+			document.getElementById("TS").innerHTML= "Transformer Size:"+TS+"KVA";
+			
+	  if (TS== 5){
+		  Cost="$"+7475.11+"USD";
+		   document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 10){
+		  Cost="$"+7921.34+"USD";
+		   document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 15){
+		  Cost="$"+8850.54+"USD";
+		   document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 25){
+		  Cost="$"+10226.66+"USD";
+		   document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 37.5){
+		  Cost="$"+13641.59+"USD";
+		   document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 50){
+		  Cost="$"+14171.88+"USD";
+		   document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 75){
+		  Cost="$"+15308.08+"USD";
+		  document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 100){
+		  Cost="$"+18556.83+"USD";
+		 document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 167){
+		  Cost="$"+21021.43+"USD";
+		 document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  
+     
+	}
+		}else if((ESDF < 105 || ESDF == 105 && ESDF > 70) && (EWDF < 120 || EWDF == 120 && EWDF > 80)){
+			TS = 75;
+			document.getElementById("TS").innerHTML= "Transformer Size:"+TS+"KVA";
+			console.log(TS);
+			
+	  if (TS== 5){
+		  Cost="$"+7475.11+"USD";
+		   document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 10){
+		  Cost="$"+7921.34+"USD";
+		   document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 15){
+		  Cost="$"+8850.54+"USD";
+		   document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 25){
+		  Cost="$"+10226.66+"USD";
+		   document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 37.5){
+		  Cost="$"+13641.59+"USD";
+		   document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 50){
+		  Cost="$"+14171.88+"USD";
+		   document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 75){
+		  Cost="$"+15308.08+"USD";
+		  document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 100){
+		  Cost="$"+18556.83+"USD";
+		 document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 167){
+		  Cost="$"+21021.43+"USD";
+		 document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  
+     
+	}
+		}else if((ESDF < 140 || ESDF == 140 && ESDF > 105) && (EWDF < 160 || EWDF == 160 && EWDF > 120)){
+			TS = 100;
+			document.getElementById("TS").innerHTML= "Transformer Size:"+TS+"KVA";
+			console.log(TS);
+			
+	  if (TS== 5){
+		  Cost="$"+7475.11+"USD";
+		   document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 10){
+		  Cost="$"+7921.34+"USD";
+		   document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 15){
+		  Cost="$"+8850.54+"USD";
+		   document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 25){
+		  Cost="$"+10226.66+"USD";
+		   document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 37.5){
+		  Cost="$"+13641.59+"USD";
+		   document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 50){
+		  Cost="$"+14171.88+"USD";
+		   document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 75){
+		  Cost="$"+15308.08+"USD";
+		  document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 100){
+		  Cost="$"+18556.83+"USD";
+		 document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 167){
+		  Cost="$"+21021.43+"USD";
+		 document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  
+     
+	}
+		}else if((ESDF < 234 || ESDF == 234 && ESDF > 140) && (EWDF < 267 || EWDF == 267 && EWDF > 160)){
+			TS = 167;
+			document.getElementById("TS").innerHTML= "Transformer Size:"+TS+"KVA";
+			console.log(TS);
+			console.log(ESDF);
+			console.log(EWDF);
+			
+	  if (TS== 5){
+		  Cost="$"+7475.11+"USD";
+		   document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 10){
+		  Cost="$"+7921.34+"USD";
+		   document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 15){
+		  Cost="$"+8850.54+"USD";
+		   document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 25){
+		  Cost="$"+10226.66+"USD";
+		   document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 37.5){
+		  Cost="$"+13641.59+"USD";
+		   document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 50){
+		  Cost="$"+14171.88+"USD";
+		   document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 75){
+		  Cost="$"+15308.08+"USD";
+		  document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 100){
+		  Cost="$"+18556.83+"USD";
+		 document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  }else if (TS== 167){
+		  Cost="$"+21021.43+"USD";
+		 document.getElementById("COST").innerHTML="The Transformer will Cost:"+Cost;	
+	  
+     
+	}
+		}
+		
+		
+		
+		
+	} 
+	   
+		
+
 
 
 	if(Met=="SquareFootage Table"){
